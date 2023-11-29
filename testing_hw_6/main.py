ec47980d4f12d999b512519d2fc9edc2ab02e668
JaneMishkina <janemishkina@yandex.ru>
2023-11-29 18:43:22 +0300

    Добавлена 6 домашка

---
+++ testing_hw_6/main.py
@@ -0,0 +1,15 @@
+from list_comparison import ListComparer
+list1 = [1, 2, 3, 4, 5]
+list2 = [5, 6, 7, 8, 9]
+
+comparer = ListComparer(list1, list2)
+comparer.compare_lists()
+
+average = ListComparer(list1, list2)
+average1 = average.calculate_average(list1)
+average2 = average.calculate_average(list2)
+print("Average of list1:", average1)
+print("Average of list2:", average2)
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
