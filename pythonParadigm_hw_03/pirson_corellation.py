04b58e52f459e47f20d3cf9890703b2832a1bfc8
JaneMishkina <janemishkina@yandex.ru>
2023-12-08 00:53:03 +0300

    исправлена формула пирсона

--- pythonParadigm_hw_03/pirson_corellation.py
+++ pythonParadigm_hw_03/pirson_corellation.py
@@ -4,39 +4,27 @@
 import math
 
 
-def mean(values):
-    """Вычисляет среднее значение списка"""
-    return sum(values) / len(values)
-
-
 def pearson_correlation(x, y):
     """
         Вычисляет корреляцию Пирсона между двумя массивами
         Возвращает:
         correlation -- значение корреляции Пирсона
         """
+
     n = len(x)
-    sum_xy = sum(xi * yi for xi, yi in zip(x, y))
-    sum_x = sum(x)
-    sum_y = sum(y)
-    sum_x_squared = sum(xi ** 2 for xi in x)
-    sum_y_squared = sum(yi ** 2 for yi in y)
-
-    numerator = (n * sum_xy) - (sum_x * sum_y)
-    denominator = math.sqrt((n * sum_x_squared - sum_x ** 2) * (n * sum_y_squared - sum_y ** 2))
-
+    median_x = sum(x) / n  # среднее арифметическое массива х
+    median_y = sum(y) / n  # среднее арифметическое массива у
+    numerator = sum([(xi - median_x) * (yi - median_y) for xi, yi in zip(x, y)])
+    denominator = math.sqrt(sum([((xi - median_x) ** 2) * ((yi - median_y) ** 2) for xi, yi in zip(x, y)]))
     correlation = numerator / denominator
     return correlation
 
 
 # Пример использования
-x = [1, 2, 3, 4, 5]
-y = [2, 4, 6, 8, 10]
+x = [1, 2, 3, 4, 6]
+y = [2, 4, 6, 8, 11]
 
 correlation = pearson_correlation(x, y)
 print("Корреляция Пирсона:", correlation)
 
-"""В этом примере используются две функции: mean, которая вычисляет среднее значение массива, и pearson_correlation, 
-которая реализует формулу корреляции Пирсона. После определения этих функций, можно передать свои массивы x и y 
-функции pearson_correlation, чтобы вычислить корреляцию, а затем вывести результат на экран."""
-"""Этот скрипт оценивает только линейную корреляцию между двумя случайными величинами."""
+
